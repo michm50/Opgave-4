dfedadd157f86ae8b75302a74da5d53922cd9f28
michm50 <74715453+michm50@users.noreply.github.com>
2021-09-26 21:40:39 +0200

    Initial commit

---
+++ FootballPlayerAPI/Controllers/FootballPlayerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sport;
+
+namespace FootballPlayerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayerController : ControllerBase
+    {
+        private Managers.FootballPlayerManager manager = new Managers.FootballPlayerManager();
+
+        [HttpGet]
+        public List<FootballPlayer> Get()
+        {
+            return manager.Get();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return manager.Get(id);
+        }
+
+        [HttpGet]
+        [Route("Name/{name}")]
+        public List<FootballPlayer> Get(string name)
+        {
+            return manager.GetName(name);
+        }
+
+        [HttpPost]
+        public bool Post([FromBody] FootballPlayer player)
+        {
+            return manager.Create(player);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public bool Put(int id, [FromBody] FootballPlayer player)
+        {
+            return manager.Update(id, player);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return manager.Delete(id);
+        }
+    }
+}
---
+++ FootballPlayerAPI/Managers/FootballPlayerManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sport;
+
+namespace FootballPlayerAPI.Managers
+{
+    public class FootballPlayerManager
+    {
+        private static List<FootballPlayer> _players = new List<FootballPlayer>()
+        {
+            new FootballPlayer(1, "Martin Hansen", 149.00m, 22),
+            new FootballPlayer(2, "Foliak Chunkyboi", 243.88m, 99),
+            new FootballPlayer(3, "Richard Fawkes", 399.00m, 48),
+            new FootballPlayer(4, "Helle etellerandet", 2315782345.00m, 1)
+        };
+
+        public List<FootballPlayer> Get()
+        {
+            return new List<FootballPlayer>(_players);
+        }
+
+        public FootballPlayer Get(int id)
+        {
+            return _players.Find(p => p.Id == id);
+        }
+
+        public List<FootballPlayer> GetName(string name)
+        {
+            return _players.FindAll(p => p.Name.ToLower().StartsWith(name.ToLower()));
+        }
+
+        public bool Create(FootballPlayer player)
+        {
+            if (Get(player.Id) != null)
+            {
+                _players.Add(player);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Update(int id, FootballPlayer player)
+        {
+            if (Get(id) != null)
+            {
+                _players[_players.FindIndex(i => i.Id == id)] = player;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            if (Get(id) != null)
+            {
+                FootballPlayer player = Get(id);
+                _players.Remove(player);
+                return player;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
